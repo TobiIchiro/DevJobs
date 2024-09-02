e1e3ab71d4541113187646e776c86c161125ad11
Israel <israel.toledo@outlook.es>
2024-09-02 10:20:23 -0600

    Comienzo del proyecto, creción del servidor, creación del rauter, habilitado Ahndlebars como Template engine

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import {engine} from 'express-handlebars';
+import router from './routes/index.js';
+
+const app = express();
+
+
+//Definiendo handlebars como templete engine
+app.engine('handlebars',engine({
+    defaultLayout: 'main'
+}))
+app.set('view engine','handlebars')
+app.set('views','./views')
+
+//static files
+
+
+app.use('/',router)
+
+const puerto = process.env.PUERTO || 5000;
+app.listen(puerto, () => {
+    console.log(`El servidor funciona en el puerto ${puerto}`)
+})
